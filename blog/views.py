08be3f0e8541c14830ecde6ff61d15d6fa10aaa1
NewTechCoder <ayanda.latsha@gmail.com>
2021-03-10 08:13:03 +0200

    initil commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+# blog/view.py
+from django.views.generic import DetailView, ListView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
